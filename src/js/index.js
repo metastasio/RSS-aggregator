0b5c3ad5f292a277dbf29e08c2570d499265c92c
metastasio <myaldzina.m@gmail.com>
2023-07-19 18:37:09 +0300

    Modified

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,45 @@
 import '../scss/styles.scss';
+import * as yup from 'yup';
+import onChange from 'on-change';
 
-console.log('Hello World!');
-console.log('AAAAAAAAAAAAAAAAAAAAAAAAAAAAA');
+const re =
+  /^((ftp|http|https):\/\/)?(www.)?(?!.*(ftp|http|https|www.))[a-zA-Z0-9_-]+(\.[a-zA-Z]+)+((\/)[\w#]+)*(\/\w+\?[a-zA-Z0-9_]+=\w+(&[a-zA-Z0-9_]+=\w+)*)?$/gm;
+
+const schema = yup.object().shape({
+  website: yup.string().matches(re, 'URL is not valid'),
+});
+
+const validate = (input) => {
+  try {
+    schema.validateSync(input, { abortEarly: false });
+    return {};
+  } catch (e) {
+    return e;
+  }
+};
+
+const hanleInputState = () => {};
+const render = (state) => {
+  console.log(state);
+  // console.log(state)
+};
+
+const app = () => {
+
+  const initialState = {
+    status: 'notSubmitted',
+    website: [],
+    errors: {},
+  };
+
+  const state = onChange(initialState, render(initialState));
+
+  const form = document.querySelector('form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const value = new FormData(e.target);
+    state.errors = validate(value);
+    // const objectData = Object.fromEntries(data);
+  });
+};
+app();
