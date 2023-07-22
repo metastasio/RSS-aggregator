82ec25f8f9bd79349bae1e8f6cdbc4fb3ce17b3d
metastasio <myaldzina.m@gmail.com>
2023-07-22 21:28:53 +0300

    Modified index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,9 @@
 import '../scss/styles.scss';
 import * as yup from 'yup';
 import isEmpty from 'lodash/isEmpty.js';
-// import onChange from 'on-change';
+import render from './view.js';
+import onChange from 'on-change';
 
-const form = document.querySelector('form');
 const errorMessage = document.querySelector('.feedback');
 const input = document.querySelector('input');
 
@@ -20,26 +20,6 @@ const validate = (input) => {
   }
 };
 
-const hanleInputState = () => {};
-const render = (state) => {
-  if (state.state === 'valid') {
-    document.querySelector('form').reset();
-    input.focus();
-    input.classList.remove('is-invalid');
-    input.classList.add('is-valid');
-    errorMessage.classList.remove('text-danger');
-    errorMessage.classList.add('text-success');
-    errorMessage.textContent = 'RSS was added to the feed';
-  } else if (state.state === 'invalid') {
-    input.classList.remove('is-valid');
-    input.classList.add('is-invalid');
-    errorMessage.classList.remove('text-success');
-    errorMessage.classList.add('text-danger');
-    errorMessage.textContent = state.errors.message;
-  }
-  console.log(state);
-};
-
 const app = () => {
   const state = {
     status: 'notSubmitted',
@@ -48,24 +28,34 @@ const app = () => {
     state: '',
   };
 
+  const watchedState = onChange(state, render);
+
+  const form = document.querySelector('form');
   form.addEventListener('submit', (e) => {
     e.preventDefault();
-    console.log(errorMessage);
     const formData = new FormData(e.target);
     const objectData = Object.fromEntries(formData);
-    if (state.feed.includes(formData.get('url'))) {
-      state.state = 'invalid';
-      state.errors = { message: 'Has already been added' };
-      render(state);
+    if (watchedState.feed.includes(formData.get('url'))) {
+      input.classList.remove('is-valid');
+      input.classList.add('is-invalid');
+      errorMessage.classList.remove('text-success');
+      errorMessage.classList.add('text-danger');
+      watchedState.state = 'invalid';
+      watchedState.errors = { message: 'This URL has already been added' };
     } else {
-      state.errors = validate(objectData);
+      watchedState.errors = validate(objectData);
       if (isEmpty(state.errors)) {
-        state.state = 'valid';
-        state.feed.push(formData.get('url'));
+        document.querySelector('form').reset();
+        input.focus();
+        input.classList.remove('is-invalid');
+        input.classList.add('is-valid');
+        errorMessage.classList.remove('text-danger');
+        errorMessage.classList.add('text-success');
+        watchedState.state = 'valid';
+        watchedState.feed.push(formData.get('url'));
       } else {
-        state.state = 'invalid';
+        watchedState.state = 'invalid';
       }
-      render(state);
     }
   });
 };
