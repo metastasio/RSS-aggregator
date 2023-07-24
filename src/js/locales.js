bc523749484daa8adefcdc4365759f8750e07330
metastasio <myaldzina.m@gmail.com>
2023-07-24 16:32:02 +0300

    Added locales

---
+++ src/js/locales.js
@@ -0,0 +1,9 @@
+export default {
+  en: {
+    translation: {
+      success: 'RSS was added to the list',
+      incorrectURL: 'URL must be a valid URL',
+      double: 'This URL has already been added',
+    },
+  },
+};
