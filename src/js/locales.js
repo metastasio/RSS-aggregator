82c7b0220cfb69fd8266dc224642fc4015fd6977
metastasio <myaldzina.m@gmail.com>
2023-07-24 19:07:57 +0300

    Deleted src/js/locales.js

--- src/js/locales.js
+++
@@ -1,9 +0,0 @@
-export default {
-  en: {
-    translation: {
-      success: 'RSS was added to the list',
-      incorrectURL: 'URL must be a valid URL',
-      double: 'This URL has already been added',
-    },
-  },
-};
