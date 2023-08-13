2f16965c26489d969c17e4804a8e98e85353cfaa
metastasio <myaldzina.m@gmail.com>
2023-08-13 13:51:46 +0300

    Fixed linter errors

--- src/js/parser.js
+++ src/js/parser.js
@@ -1,42 +1,38 @@
 import newInstance from './locales/index.js';
 
-
 const parser = new DOMParser();
 
 const rssParser = (data, url) => {
-const parsed = parser.parseFromString(
-      data.data.contents,
-      'application/xml',
-    );
+  const parsed = parser.parseFromString(data.data.contents, 'application/xml');
 
-    if (parsed.querySelector('parsererror')) {
-      return { message: newInstance.t('noRSS') };
-    }
-    const titleElement = parsed.querySelector('title');
-    const feedTitle = titleElement.textContent;
-    const descriptionElement = parsed.querySelector('description');
-    const feedDescription = descriptionElement.textContent;
-    const itemTags = parsed.querySelectorAll('item');
-    const items = [...itemTags].map((item) => {
-      const title = item.querySelector('title');
-      const link = item.querySelector('link');
-      const description = item.querySelector('description');
-      return {
-        title: title.innerHTML
-          .trim()
-          .replace(/^(\/\/\s*)?<!\[CDATA\[|(\/\/\s*)?\]\]>$/g, ''),
-        link: link.innerHTML,
-        description: description.innerHTML
-          .trim()
-          .replace(/^(\/\/\s*)?<!\[CDATA\[|(\/\/\s*)?\]\]>$/g, ''),
-      };
-    });
-
+  if (parsed.querySelector('parsererror')) {
+    return { message: newInstance.t('noRSS') };
+  }
+  const titleElement = parsed.querySelector('title');
+  const feedTitle = titleElement.textContent;
+  const descriptionElement = parsed.querySelector('description');
+  const feedDescription = descriptionElement.textContent;
+  const itemTags = parsed.querySelectorAll('item');
+  const items = [...itemTags].map((item) => {
+    const title = item.querySelector('title');
+    const link = item.querySelector('link');
+    const description = item.querySelector('description');
     return {
-      title: feedTitle,
-      description: feedDescription,
-      link: url,
-      items,
+      title: title.innerHTML
+        .trim()
+        .replace(/^(\/\/\s*)?<!\[CDATA\[|(\/\/\s*)?\]\]>$/g, ''),
+      link: link.innerHTML,
+      description: description.innerHTML
+        .trim()
+        .replace(/^(\/\/\s*)?<!\[CDATA\[|(\/\/\s*)?\]\]>$/g, ''),
     };
+  });
+
+  return {
+    title: feedTitle,
+    description: feedDescription,
+    link: url,
+    items,
+  };
 };
 export default rssParser;
