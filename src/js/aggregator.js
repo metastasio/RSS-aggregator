ca2605a62a4c25096b1cf97d5bda95497d958c17
metastasio <myaldzina.m@gmail.com>
2023-08-13 15:40:10 +0300

    Separated URL obtaining into a function

--- src/js/aggregator.js
+++ src/js/aggregator.js
@@ -2,13 +2,18 @@ import axios from 'axios';
 import newInstance from './locales/index.js';
 import rssParser from './parser.js';
 
-const aggregator = (url) => axios
-  .get(
+const getLink = (url) => {
+  const resultUrl = new URL(
     `https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(
       url,
     )}`,
-    { timeout: 5000 },
-  )
-  .then((data) => rssParser(data, url))
-  .catch(() => ({ message: newInstance.t('networkError') }));
+  );
+  return resultUrl;
+};
+
+const aggregator = (url) =>
+  axios
+    .get(getLink(url), { timeout: 5000 })
+    .then((data) => rssParser(data, url))
+    .catch(() => ({ message: newInstance.t('networkError') }));
 export default aggregator;
